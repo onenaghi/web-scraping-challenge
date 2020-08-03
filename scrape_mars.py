022138d0e452ea62536e7d3374607bcedcc7fe63
onenaghi <onenaghi@yahoo.com>
2020-08-02 22:56:11 -0500

    added python flask and MangoDB files

---
+++ scrape_mars.py
@@ -0,0 +1,24 @@
+# Dependencies
+import pandas as pd
+import requests as req
+import time
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+from selenium import webdriver
+
+def init_browser():
+    executable_path = {"executable_path": "chromedriver.exe"}
+    return Browser("chrome", **executable_path, headless=False)
+
+def scrape():
+    browser = init_browser()
+    mars_dict ={}
+
+    # Mars News URL of page to be scraped
+    news_url = 'https://mars.nasa.gov/news/'
+    browser.visit(news_url)
+    html = browser.html
+    news_soup = BeautifulSoup(html, 'html.parser')
+    # Retrieve the latest news title and paragraph
+    news_title = news_soup.find_all('div', class_='content_title')[0].text
+    news_p = news_soup.find_all('div', class_='article_teaser_body')[0].text
